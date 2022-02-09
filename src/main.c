9bd79dfda4330effbf3eafaeb43187b8e01333c9
serdenvl <serdenvl@gmail.com>
2022-02-09 16:41:02 +0700

    +coments +free

--- src/main.c
+++ src/main.c
@@ -2,28 +2,35 @@
 #include <stdlib.h>
 #include "utils.h"
 
+// преобразует в отсортированный массив без повторений
 int* to_setlike(int* array, int length)
 {
     int* s_array = new_array(length+1);
-    s_array[0] = 1;
+    s_array[0] = 1; // отводится под длину массива
     s_array[1] = array[0];
 
     for(int i = 1; i < length; ++i)
     {
         for(int j = 1; j <= s_array[0]; ++j)
         {
+            // элемент i уже находится в массиве
             if(array[i] == s_array[j])
                 break;
 
+            // j - место элемента i в отсортированном массиве 
             if(array[i] < s_array[j])
             {
-                for(int k = s_array[0]+1; k != j; --k)
-                    swap(&s_array[k-1], &s_array[k]);
-                s_array[j] = array[i];
+                // освобождение места под элемент
                 s_array[0] += 1;
+                for(int k = s_array[0]; k != j; --k)
+                    swap(&s_array[k-1], &s_array[k]);
+                
+                s_array[j] = array[i];
                 break;
             }
 
+            // если иное условие не отработало
+            // добавление элемента i в конец
             if(j == s_array[0])
             {
                 s_array[0] += 1;
@@ -32,9 +39,11 @@ int* to_setlike(int* array, int length)
             }
         }
     }
+
     return resize_array(s_array, s_array[0]+1);
 }
 
+// проверка на схожесть
 int setlike_equal(int* arrA, int* arrB)
 {
     for(int i = 0; i <= arrA[0]; ++i)
@@ -43,6 +52,7 @@ int setlike_equal(int* arrA, int* arrB)
     return 1;
 }
 
+// подсчет количества схожих строк
 int num_similar_string(int** matrix, int M, int N)
 {
     int* first = to_setlike(matrix[0], N);
@@ -56,6 +66,7 @@ int num_similar_string(int** matrix, int M, int N)
         free(buff);
     }
 
+    free(first);
     return count;
 }
 
