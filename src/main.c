d9b543f3d43b38860c3802a03994c4c56bed6c72
serdenvl <serdenvl@gmail.com>
2022-02-09 15:00:02 +0700

    +all

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "utils.h"
 
 /*
     *place_for_task*
@@ -7,15 +8,84 @@
 
 #define task_name "lab #1"
 #define welcome_message "\
-Дана матрица MxN. \
-Вывести количество похожих строк. \
-Строки похожи, если множества их элементов эквивалентны. \
+Дана матрица MxN. \n\
+Вывести количество строк, схожих с первой. \n\
+Строки похожи, если множества их элементов эквивалентны. \n\
 "
+
+int* to_setlike(int* array, int length)
+{
+    int* s_array = new_array(length+1);
+    s_array[0] = 1;
+    s_array[1] = array[0];
+
+    for(int i = 1; i < length; ++i)
+    {
+        for(int j = 1; j <= s_array[0]; ++j)
+        {
+            if(array[i] == s_array[j])
+                break;
+
+            if(array[i] < s_array[j])
+            {
+                for(int k = s_array[0]+1; k != j; --k)
+                    swap(&s_array[k-1], &s_array[k]);
+                s_array[j] = array[i];
+                s_array[0] += 1;
+                break;
+            }
+
+            if(j == s_array[0])
+            {
+                s_array[0] += 1;
+                s_array[s_array[0]] = array[i];
+                break;
+            }
+        }
+    }
+    return resize_array(s_array, s_array[0]+1);
+}
+
+int setlike_equal(int* arrA, int* arrB)
+{
+    for(int i = 0; i <= arrA[0]; ++i)
+        if(arrA[i] != arrB[i])
+            return 0;
+    return 1;
+}
+
+int num_similar_string(int** matrix, int M, int N)
+{
+    int* first = to_setlike(matrix[0], N);
+    int count = 0;
+
+    int* buff;
+    for(int i = 1; i < M; ++i)
+    {
+        buff = to_setlike(matrix[i], N);
+        count += setlike_equal(first, buff);
+        free(buff);
+    }
+
+    return count;
+}
 
 int main()
 {
     localize();
     welcome(task_name, welcome_message);
 
+    int M = scan_int("M: ");
+    int N = scan_int("N: ");
+    int** matrix = new_matrix(M, N);
+    printf("%d", matrix[0][0]);
+    input_matrix(matrix, M, N);
+
+    printf("Матрица: ");
+    print_matrix(matrix, M, N);
+
+    printf("\nКоличество строк, схожих с первой: %d", num_similar_string(matrix, M, N));
+
+    free_matrix(matrix, M);
     getchar();
 }
